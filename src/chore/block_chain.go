7bdb21eb03677baaf3c6b8462ecb23b4b577f05b
Jeremy Pansier <jeremy.pansier@olea-medical.com>
2022-07-05 16:40:01 +0200

    lesson 14 calculate total

--- src/chore/block_chain.go
+++ src/chore/block_chain.go
@@ -2,24 +2,27 @@ package chore
 
 import (
 	"fmt"
+	"log"
 	"strings"
 )
 
-const MiningDifficulty = 3
+const (
+	MiningDifficulty = 3
+	MiningSender     = "THE BLOCK CHAIN"
+	MiningReward     = 1.
+)
 
 type BlockChain struct {
 	transactions []*Transaction
 	blocks       []*Block
+	address      string
 }
 
-func NewBlockChain() *BlockChain {
+func NewBlockChain(address string) *BlockChain {
 	blockchain := new(BlockChain)
-	blockchain.CreateBlock(0, new(Block).Hash())
+	blockchain.address = address
+	blockchain.createBlock(0, new(Block).Hash())
 	return blockchain
-}
-
-func (blockChain *BlockChain) LastBlock() *Block {
-	return blockChain.blocks[len(blockChain.blocks)-1]
 }
 
 func (blockChain *BlockChain) Print() {
@@ -30,19 +33,32 @@ func (blockChain *BlockChain) Print() {
 	fmt.Printf("%s\n", strings.Repeat("*", 60))
 }
 
-func (blockChain *BlockChain) CreateBlock(nonce int, previousHash [32]byte) *Block {
+func (blockChain *BlockChain) AddTransaction(sender string, recipient string, value float32) {
+	transaction := NewTransaction(sender, recipient, value)
+	blockChain.transactions = append(blockChain.transactions, transaction)
+}
+
+func (blockChain *BlockChain) Mining() bool {
+	blockChain.AddTransaction(MiningSender, blockChain.address, MiningReward)
+	nonce := blockChain.proofOfWork()
+	lastHash := blockChain.lastBlock().Hash()
+	blockChain.createBlock(nonce, lastHash)
+	log.Println("action=mining, status=success")
+	return true
+}
+
+func (blockChain *BlockChain) createBlock(nonce int, previousHash [32]byte) *Block {
 	block := NewBlock(nonce, previousHash, blockChain.transactions)
 	blockChain.blocks = append(blockChain.blocks, block)
 	blockChain.transactions = []*Transaction{}
 	return block
 }
 
-func (blockChain *BlockChain) AddTransaction(sender string, recipient string, value float32) {
-	transaction := NewTransaction(sender, recipient, value)
-	blockChain.transactions = append(blockChain.transactions, transaction)
+func (blockChain *BlockChain) lastBlock() *Block {
+	return blockChain.blocks[len(blockChain.blocks)-1]
 }
 
-func (blockChain *BlockChain) CopyTransactions() []*Transaction {
+func (blockChain *BlockChain) copyTransactions() []*Transaction {
 	transactions := make([]*Transaction, 0)
 	for _, transaction := range blockChain.transactions {
 		transactions = append(transactions,
@@ -53,19 +69,36 @@ func (blockChain *BlockChain) CopyTransactions() []*Transaction {
 	return transactions
 }
 
-func (blockChain *BlockChain) ValidProof(nonce int, previousHash [32]byte, transactions []*Transaction, difficulty int) bool {
+func (blockChain *BlockChain) validProof(nonce int, previousHash [32]byte, transactions []*Transaction, difficulty int) bool {
 	zeros := strings.Repeat("0", difficulty)
 	guessBlock := Block{0, nonce, previousHash, transactions}
 	guessHashStr := fmt.Sprintf("%x", guessBlock.Hash())
 	return guessHashStr[:difficulty] == zeros
 }
 
-func (blockChain *BlockChain) ProofOfWork() int {
-	transactions := blockChain.CopyTransactions()
-	lastHash := blockChain.LastBlock().Hash()
+func (blockChain *BlockChain) proofOfWork() int {
+	transactions := blockChain.copyTransactions()
+	lastHash := blockChain.lastBlock().Hash()
 	var nonce int
-	for !blockChain.ValidProof(nonce, lastHash, transactions, MiningDifficulty) {
+	for !blockChain.validProof(nonce, lastHash, transactions, MiningDifficulty) {
 		nonce++
 	}
 	return nonce
+}
+
+func (blockChain *BlockChain) CalculateTotalAmount(address string) float32 {
+	var totalAmount float32 = 0.0
+	for _, block := range blockChain.blocks {
+		for _, transaction := range block.transactions {
+			value := transaction.value
+			if address == transaction.recipient {
+				totalAmount += value
+			}
+
+			if address == transaction.sender {
+				totalAmount -= value
+			}
+		}
+	}
+	return totalAmount
 }
--- src/main.go
+++ src/main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"ruthenium/src/chore"
 )
 
 func main() {
-	blockChain := chore.NewBlockChain()
+	myBlockChainAddress := "my block chain address"
+	blockChain := chore.NewBlockChain(myBlockChainAddress)
 	blockChain.Print()
 
-	blockChain.AddTransaction("A", "B", 1.)
-
-	lastHash := blockChain.LastBlock().Hash()
-	nonce := blockChain.ProofOfWork()
-	blockChain.CreateBlock(nonce, lastHash)
+	blockChain.AddTransaction("A", "B", 100.)
+	blockChain.Mining()
+	blockChain.AddTransaction("B", "C", 10.)
+	blockChain.Mining()
 	blockChain.Print()
-
-	lastHash = blockChain.LastBlock().Hash()
-	nonce = blockChain.ProofOfWork()
-	blockChain.CreateBlock(nonce, lastHash)
-	blockChain.Print()
+	fmt.Printf("A %.1f\n", blockChain.CalculateTotalAmount("A"))
+	fmt.Printf("B %.1f\n", blockChain.CalculateTotalAmount("B"))
+	fmt.Printf("C %.1f\n", blockChain.CalculateTotalAmount("C"))
+	fmt.Printf("mine %.1f\n", blockChain.CalculateTotalAmount("my block chain address"))
 }
