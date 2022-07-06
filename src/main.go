51a49570b1e12ddb3a3fc412adf43641b14b5025
Jeremy Pansier <jeremy.pansier@olea-medical.com>
2022-07-06 06:53:50 +0200

    lesson 21 create the server

--- src/main.go
+++ src/main.go
@@ -1,29 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"ruthenium/src/chain"
+	"ruthenium/src/server"
 )
 
 func main() {
-	// Wallet
-	walletA := chain.NewWallet()
-	walletB := chain.NewWallet()
-	walletC := chain.NewWallet()
-	minerWallet := chain.NewWallet()
-
-	// Block chain
-	blockChain := chain.NewBlockchain(minerWallet.Address())
-	isAdded1 := blockChain.AddTransaction(walletA, walletB.Address(), 100.)
-	fmt.Println("Transaction 1 added? ", isAdded1)
-	blockChain.Mining()
-
-	isAdded2 := blockChain.AddTransaction(walletB, walletC.Address(), 10.)
-	fmt.Println("Transaction 2 added? ", isAdded2)
-	blockChain.Mining()
-
-	fmt.Printf("A %.1f\n", blockChain.CalculateTotalAmount(walletA.Address()))
-	fmt.Printf("B %.1f\n", blockChain.CalculateTotalAmount(walletB.Address()))
-	fmt.Printf("C %.1f\n", blockChain.CalculateTotalAmount(walletC.Address()))
-	fmt.Printf("mine %.1f\n", blockChain.CalculateTotalAmount(minerWallet.Address()))
+	port := flag.Uint("port", 5000, "TCP port number for blockchain server")
+	flag.Parse()
+	app := server.NewServer(uint16(*port))
+	fmt.Println("Running...")
+	app.Run()
 }
---
+++ test/chain/blockchain_test.go
@@ -0,0 +1,40 @@
+package chain
+
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+	"ruthenium/src/chain"
+	"testing"
+)
+
+func Test_Blockchain(t *testing.T) {
+	// Wallet
+	walletA := chain.NewWallet()
+	walletB := chain.NewWallet()
+	walletC := chain.NewWallet()
+	minerWallet := chain.NewWallet()
+
+	// Block chain
+	blockChain := chain.NewBlockchain(minerWallet.Address())
+	isAdded1 := blockChain.AddTransaction(walletA, walletB.Address(), 100.)
+	assert(t, isAdded1, "Failed to add first transaction")
+	blockChain.Mining()
+
+	isAdded2 := blockChain.AddTransaction(walletB, walletC.Address(), 10.)
+	assert(t, isAdded2, "Failed to add second transaction")
+	blockChain.Mining()
+
+	fmt.Printf("A %.1f\n", blockChain.CalculateTotalAmount(walletA.Address()))
+	fmt.Printf("B %.1f\n", blockChain.CalculateTotalAmount(walletB.Address()))
+	fmt.Printf("C %.1f\n", blockChain.CalculateTotalAmount(walletC.Address()))
+	fmt.Printf("mine %.1f\n", blockChain.CalculateTotalAmount(minerWallet.Address()))
+}
+
+func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
+	if !condition {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
+		tb.FailNow()
+	}
+}
