f36a5d75f4d8b056987d0b81b601aedaf0c3cc61
Jeremy Pansier <jeremy.pansier@olea-medical.com>
2022-07-31 09:53:53 +0200

    change log level

--- src/chain/blockchain.go
+++ src/chain/blockchain.go
@@ -308,9 +308,9 @@ func (blockchain *Blockchain) ResolveConflicts() {
 			blockchain.blockResponses = longestChainResponse
 			blockchain.blocks = longestChain
 			blockchain.clearTransactions()
-			blockchain.logger.Info("Conflicts resolved: blockchain replaced")
+			blockchain.logger.Warn("Conflicts resolved: blockchain replaced")
 		}
-		blockchain.logger.Info("Conflicts resolved: blockchain kept")
+		blockchain.logger.Warn("Conflicts resolved: blockchain kept")
 	}(blockchain.neighbors)
 }
 
