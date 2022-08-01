6899b6f1bcaf29c436c94711bb54abd0d0e68bed
Jeremy Pansier <jeremy.pansier@olea-medical.com>
2022-08-01 18:21:45 +0200

    try fixing timeout

--- src/chain/blockchain.go
+++ src/chain/blockchain.go
@@ -136,7 +136,9 @@ func (blockchain *Blockchain) FindNeighbors() {
 				neighborIp := neighbor.Ip()
 				neighborPort := neighbor.Port()
 				if neighborIp != *targetRequest.Ip || neighborPort != *targetRequest.Port {
-					_ = neighbor.SendTarget(targetRequest)
+					go func() {
+						_ = neighbor.SendTarget(targetRequest)
+					}()
 				}
 			}
 		}
