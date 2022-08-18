61b78cd16c5e707b53b5df19ea870f2012e0d20f
Jeremy Pansier <jeremy.pansier@olea-medical.com>
2022-08-18 18:26:32 +0200

    refactor and add unit test

--- test/node/blockchain/service_test.go
+++ test/node/blockchain/service_test.go
@@ -8,11 +8,12 @@ import (
 	"ruthenium/src/node/authentication"
 	"ruthenium/src/node/blockchain"
 	"ruthenium/src/node/blockchain/mining"
+	"sync"
 	"testing"
 	"time"
 )
 
-func Test_Blockchain(t *testing.T) {
+func Test_AddTransaction_Allowed_TransactionAdded(t *testing.T) {
 	// Arrange
 	minerWallet, _ := authentication.NewWallet("", "")
 	minerWalletAddress := minerWallet.Address()
@@ -21,33 +22,65 @@ func Test_Blockchain(t *testing.T) {
 	walletB, _ := authentication.NewWallet("", "")
 	walletBAddress := walletB.Address()
 	logger := log.NewLogger(log.Error)
-	blockChain := blockchain.NewService(minerWalletAddress, "", 8106, time.Nanosecond, logger)
-	wg := blockChain.WaitGroup()
+	service := blockchain.NewService(minerWalletAddress, "", 8106, time.Nanosecond, logger)
 
 	// Act
-	var value1 uint64 = 40 * blockchain.ParticlesCount
-	transaction1 := mining.NewTransaction(0, minerWalletAddress, walletAAddress, value1)
+	var amount1 uint64 = 40 * blockchain.ParticlesCount
+	transaction1 := mining.NewTransaction(0, minerWalletAddress, walletAAddress, amount1)
 	signature1, _ := transaction1.Sign(minerWallet.PrivateKey())
-	blockChain.AddTransaction(transaction1, minerWallet.PublicKey(), signature1)
-	wg.Wait()
-	blockChain.Mine()
-	wg.Wait()
+	addTransaction(service, minerWallet.PublicKey(), transaction1, signature1)
 
-	var value2 uint64 = 10 * blockchain.ParticlesCount
-	transaction2 := mining.NewTransaction(0, walletAAddress, walletBAddress, value2)
+	var amount2 uint64 = 10 * blockchain.ParticlesCount
+	transaction2 := mining.NewTransaction(0, walletAAddress, walletBAddress, amount2)
 	signature2, _ := transaction2.Sign(walletA.PrivateKey())
-	blockChain.AddTransaction(transaction2, walletA.PublicKey(), signature2)
-	wg.Wait()
-	blockChain.Mine()
-	wg.Wait()
+	addTransaction(service, walletA.PublicKey(), transaction2, signature2)
 
 	// Assert
-	expectedWalletAAmount := value1 - value2
-	actualWalletAAmount := blockChain.CalculateTotalAmount(0, walletAAddress)
+	expectedWalletAAmount := amount1 - amount2
+	actualWalletAAmount := service.CalculateTotalAmount(0, walletAAddress)
 	assert(t, expectedWalletAAmount == actualWalletAAmount, fmt.Sprintf("Wrong wallet A amount. Expected: %d - Actual: %d", expectedWalletAAmount, actualWalletAAmount))
-	expectedWalletBAmount := value2
-	actualWalletBAmount := blockChain.CalculateTotalAmount(0, walletBAddress)
+	expectedWalletBAmount := amount2
+	actualWalletBAmount := service.CalculateTotalAmount(0, walletBAddress)
 	assert(t, expectedWalletBAmount == actualWalletBAmount, fmt.Sprintf("Wrong wallet B amount. Expected: %d - Actual: %d", expectedWalletBAmount, actualWalletBAmount))
+}
+
+func Test_AddTransaction_Allowed_TransactionNotAdded(t *testing.T) {
+	// Arrange
+	minerWallet, _ := authentication.NewWallet("", "")
+	minerWalletAddress := minerWallet.Address()
+	walletA, _ := authentication.NewWallet("", "")
+	walletAAddress := walletA.Address()
+	walletB, _ := authentication.NewWallet("", "")
+	walletBAddress := walletB.Address()
+	logger := log.NewLogger(log.Error)
+	service := blockchain.NewService(minerWalletAddress, "", 8106, time.Nanosecond, logger)
+
+	// Act
+	var amount1 uint64 = 40 * blockchain.ParticlesCount
+	transaction1 := mining.NewTransaction(0, minerWalletAddress, walletAAddress, amount1)
+	signature1, _ := transaction1.Sign(minerWallet.PrivateKey())
+	addTransaction(service, minerWallet.PublicKey(), transaction1, signature1)
+
+	var amount2 uint64 = 10 * blockchain.ParticlesCount
+	transaction2 := mining.NewTransaction(0, walletAAddress, walletBAddress, amount2)
+	signature2, _ := transaction2.Sign(walletB.PrivateKey())
+	addTransaction(service, walletA.PublicKey(), transaction2, signature2)
+
+	// Assert
+	actualWalletAAmount := service.CalculateTotalAmount(0, walletAAddress)
+	assert(t, amount1 == actualWalletAAmount, fmt.Sprintf("Wrong wallet A amount. Expected: %d - Actual: %d", amount1, actualWalletAAmount))
+	var expectedWalletBAmount uint64 = 0
+	actualWalletBAmount := service.CalculateTotalAmount(0, walletBAddress)
+	assert(t, expectedWalletBAmount == actualWalletBAmount, fmt.Sprintf("Wrong wallet B amount. Expected: %d - Actual: %d", expectedWalletBAmount, actualWalletBAmount))
+}
+
+func addTransaction(blockchain *blockchain.Service, senderWalletPublicKey *authentication.PublicKey, transaction *mining.Transaction, signature *authentication.Signature) *sync.WaitGroup {
+	blockchain.AddTransaction(transaction, senderWalletPublicKey, signature)
+	wg := blockchain.WaitGroup()
+	wg.Wait()
+	blockchain.Mine()
+	wg.Wait()
+	return wg
 }
 
 func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
