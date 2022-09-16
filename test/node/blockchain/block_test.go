299742fa56f37d292f570a358712adfe372e7043
Jeremy Pansier <jeremypansier@gmail.com>
2022-09-16 07:49:01 +0000

    feat(blockchain): verify neighbors requests #13

--- test/node/blockchain/block_test.go
+++ test/node/blockchain/block_test.go
@@ -7,18 +7,18 @@ import (
 )
 
 func Test_IsProofOfHumanityValid_Invalid_ReturnsFalse(t *testing.T) {
-	senderAddress := "A"
-	recipientAddress := "0x1234567890123456789012345678901234567890"
-	transactions := []*blockchain.Transaction{blockchain.NewTransaction(0, senderAddress, recipientAddress, 1)}
+	senderAddress := blockchain.RewardSenderAddress
+	recipientAddress := "0x0000000000000000000000000000000000000001"
+	transactions := []*blockchain.Transaction{blockchain.NewTransaction(recipientAddress, senderAddress, nil, 0, 1)}
 	block := blockchain.NewBlock(0, [32]byte{}, transactions)
 	err := block.IsProofOfHumanityValid()
 	test.Assert(t, err != nil, "proof of humanity is valid whereas it should not")
 }
 
 func Test_IsProofOfHumanityValid_Valid_ReturnsTrue(t *testing.T) {
-	senderAddress := "A"
-	recipientAddress := "0xD70eFbfC9cF73dC1aB5f8292E7273D916D38c916"
-	transactions := []*blockchain.Transaction{blockchain.NewTransaction(0, senderAddress, recipientAddress, 1)}
+	senderAddress := blockchain.RewardSenderAddress
+	recipientAddress := "0xf14DB86A3292ABaB1D4B912dbF55e8abc112593a"
+	transactions := []*blockchain.Transaction{blockchain.NewTransaction(recipientAddress, senderAddress, nil, 0, 1)}
 	block := blockchain.NewBlock(0, [32]byte{}, transactions)
 	err := block.IsProofOfHumanityValid()
 	test.Assert(t, err == nil, "proof of humanity is invalid whereas it should be")
