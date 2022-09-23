56bfab51652296345090924c74638779dc8c0d0c
Jérémy Pansier <jeremypansier@gmail.com>
2022-09-23 17:34:34 +0200

    feat(*): support environment variables as program argument #22

--- src/node/main.go
+++ src/node/main.go
@@ -2,17 +2,18 @@ package main
 
 import (
 	"flag"
+	"github.com/my-cloud/ruthenium/src/environment"
 	"github.com/my-cloud/ruthenium/src/log"
 	"github.com/my-cloud/ruthenium/src/node/blockchain"
 )
 
 func main() {
-	mnemonic := flag.String("mnemonic", "", "The mnemonic (optional)")
-	derivationPath := flag.String("derivation-path", "m/44'/60'/0'/0/0", "The derivation path (unused if the mnemonic is omitted)")
-	password := flag.String("password", "", "The mnemonic password (unused if the mnemonic is omitted)")
-	privateKey := flag.String("private-key", "", "The private key (will be generated if not provided)")
-	port := flag.Uint("port", blockchain.DefaultPort, "TCP port number for blockchain server")
-	logLevel := flag.String("log-level", "info", "The log level")
+	mnemonic := flag.String("mnemonic", environment.NewVariable("MNEMONIC").GetStringValue(""), "The mnemonic (optional)")
+	derivationPath := flag.String("derivation-path", environment.NewVariable("DERIVATION_PATH").GetStringValue("m/44'/60'/0'/0/0"), "The derivation path (unused if the mnemonic is omitted)")
+	password := flag.String("password", environment.NewVariable("PASSWORD").GetStringValue(""), "The mnemonic password (unused if the mnemonic is omitted)")
+	privateKey := flag.String("private-key", environment.NewVariable("PRIVATE_KEY").GetStringValue(""), "The private key (will be generated if not provided)")
+	port := flag.Uint64("port", environment.NewVariable("PORT").GetUint64Value(blockchain.DefaultPort), "TCP port number for blockchain server")
+	logLevel := flag.String("log-level", environment.NewVariable("LOG_LEVEL").GetStringValue("info"), "The log level")
 
 	flag.Parse()
 	app := blockchain.NewHost(*mnemonic, *derivationPath, *password, *privateKey, uint16(*port), log.ParseLevel(*logLevel))
