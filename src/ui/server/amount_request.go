ed10f11c160704ef6c77f90659cdb3226c77936f
Jeremy Pansier <jeremy.pansier@olea-medical.com>
2022-11-07 12:10:04 +0100

    small changes

--- src/ui/server/amount_request.go
+++
@@ -1,9 +0,0 @@
-package server
-
-type AmountRequest struct {
-	Address *string
-}
-
-func (amountRequest *AmountRequest) IsInvalid() bool {
-	return amountRequest.Address == nil || len(*amountRequest.Address) == 0
-}
