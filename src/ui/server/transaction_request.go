a2ad557b6a0842adfdbd70cf934f6ebd71bb8b0d
Jérémy Pansier <jeremypansier@gmail.com>
2023-01-10 11:13:57 +0100

    feat(ui): simplify transaction request (#170)
    BREAKING CHANGE: The pubic key has been removed from the POST /transaction request body.

--- src/ui/server/transaction_request.go
+++ src/ui/server/transaction_request.go
@@ -4,15 +4,12 @@ type TransactionRequest struct {
 	SenderPrivateKey *string `json:"sender_private_key"`
 	SenderAddress    *string `json:"sender_address"`
 	RecipientAddress *string `json:"recipient_address"`
-	// TODO remove useless field
-	SenderPublicKey *string `json:"sender_public_key"`
-	Value           *string `json:"value"`
+	Value            *string `json:"value"`
 }
 
 func (transactionRequest *TransactionRequest) IsInvalid() bool {
 	return transactionRequest.SenderPrivateKey == nil || len(*transactionRequest.SenderPrivateKey) == 0 ||
 		transactionRequest.SenderAddress == nil || len(*transactionRequest.SenderAddress) == 0 ||
 		transactionRequest.RecipientAddress == nil || len(*transactionRequest.RecipientAddress) == 0 ||
-		transactionRequest.SenderPublicKey == nil || len(*transactionRequest.SenderPublicKey) == 0 ||
 		transactionRequest.Value == nil || len(*transactionRequest.Value) == 0
 }
