67d128d9e0dba676e96413ef699e73a433a39e01
Jeremy Pansier <jeremypansier@gmail.com>
2023-01-10 10:15:11 +0100

    add tests

--- test/node/network/p2p/synchronizer_test.go
+++ test/node/network/p2p/synchronizer_test.go
@@ -12,7 +12,7 @@ import (
 	"time"
 )
 
-func Test_Synchronize_OneNeighbor_NeighborAdded(t *testing.T) {
+func Test_Synchronize_OneSeed_NeighborAdded(t *testing.T) {
 	// Arrange
 	watchMock := new(clocktest.WatchMock)
 	watchMock.NowFunc = func() time.Time { return time.Now() }
@@ -20,7 +20,7 @@ func Test_Synchronize_OneNeighbor_NeighborAdded(t *testing.T) {
 	client := new(p2ptest.ClientMock)
 	client.SendFunc = func(string, gp2p.Data) (gp2p.Data, error) { return gp2p.Data{}, nil }
 	clientFactoryMock.CreateClientFunc = func(string, uint16, string) (p2p.Client, error) { return client, nil }
-	configurationPath := "../../../"
+	configurationPath := "../../../config"
 	logger := logtest.NewLoggerMock()
 	synchronizer, _ := p2p.NewSynchronizer(0, watchMock, clientFactoryMock, configurationPath, logger)
 
@@ -31,4 +31,76 @@ func Test_Synchronize_OneNeighbor_NeighborAdded(t *testing.T) {
 	neighbors := synchronizer.Neighbors()
 	expectedNeighborsCount := 1
 	test.Assert(t, len(neighbors) == expectedNeighborsCount, fmt.Sprintf("Wrong neighbors count. Expected: %d - Actual: %d", expectedNeighborsCount, len(neighbors)))
+}
+
+func Test_Synchronize_NoConfigurationFolder_ReturnsError(t *testing.T) {
+	// Arrange
+	watchMock := new(clocktest.WatchMock)
+	watchMock.NowFunc = func() time.Time { return time.Now() }
+	clientFactoryMock := new(p2ptest.ClientFactoryMock)
+	client := new(p2ptest.ClientMock)
+	client.SendFunc = func(string, gp2p.Data) (gp2p.Data, error) { return gp2p.Data{}, nil }
+	clientFactoryMock.CreateClientFunc = func(string, uint16, string) (p2p.Client, error) { return client, nil }
+	configurationPath := ""
+	logger := logtest.NewLoggerMock()
+
+	// Act
+	_, err := p2p.NewSynchronizer(0, watchMock, clientFactoryMock, configurationPath, logger)
+
+	// Assert
+	test.Assert(t, err != nil, "No error returned whereas it should")
+}
+
+func Test_Synchronize_EmptyConfigurationFile_ReturnsError(t *testing.T) {
+	// Arrange
+	watchMock := new(clocktest.WatchMock)
+	watchMock.NowFunc = func() time.Time { return time.Now() }
+	clientFactoryMock := new(p2ptest.ClientFactoryMock)
+	client := new(p2ptest.ClientMock)
+	client.SendFunc = func(string, gp2p.Data) (gp2p.Data, error) { return gp2p.Data{}, nil }
+	clientFactoryMock.CreateClientFunc = func(string, uint16, string) (p2p.Client, error) { return client, nil }
+	configurationPath := "../../../config/emptyconfigfile"
+	logger := logtest.NewLoggerMock()
+
+	// Act
+	_, err := p2p.NewSynchronizer(0, watchMock, clientFactoryMock, configurationPath, logger)
+
+	// Assert
+	test.Assert(t, err != nil, "No error returned whereas it should")
+}
+
+func Test_Synchronize_WrongSeedFormat_ReturnsError(t *testing.T) {
+	// Arrange
+	watchMock := new(clocktest.WatchMock)
+	watchMock.NowFunc = func() time.Time { return time.Now() }
+	clientFactoryMock := new(p2ptest.ClientFactoryMock)
+	client := new(p2ptest.ClientMock)
+	client.SendFunc = func(string, gp2p.Data) (gp2p.Data, error) { return gp2p.Data{}, nil }
+	clientFactoryMock.CreateClientFunc = func(string, uint16, string) (p2p.Client, error) { return client, nil }
+	configurationPath := "../../../config/wrongseedformat"
+	logger := logtest.NewLoggerMock()
+
+	// Act
+	_, err := p2p.NewSynchronizer(0, watchMock, clientFactoryMock, configurationPath, logger)
+
+	// Assert
+	test.Assert(t, err != nil, "No error returned whereas it should")
+}
+
+func Test_Synchronize_WrongSeedPortFormat_ReturnsError(t *testing.T) {
+	// Arrange
+	watchMock := new(clocktest.WatchMock)
+	watchMock.NowFunc = func() time.Time { return time.Now() }
+	clientFactoryMock := new(p2ptest.ClientFactoryMock)
+	client := new(p2ptest.ClientMock)
+	client.SendFunc = func(string, gp2p.Data) (gp2p.Data, error) { return gp2p.Data{}, nil }
+	clientFactoryMock.CreateClientFunc = func(string, uint16, string) (p2p.Client, error) { return client, nil }
+	configurationPath := "../../../config/wrongseedportformat"
+	logger := logtest.NewLoggerMock()
+
+	// Act
+	_, err := p2p.NewSynchronizer(0, watchMock, clientFactoryMock, configurationPath, logger)
+
+	// Assert
+	test.Assert(t, err != nil, "No error returned whereas it should")
 }
