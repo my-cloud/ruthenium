2d15ec47a1eda8ab6240f11ec80d74ddf0080454
Jeremy Pansier <jeremypansier@gmail.com>
2023-09-14 21:51:18 +0200

    improve test

--- test/node/clock/tick/engine_test.go
+++ test/node/clock/tick/engine_test.go
@@ -28,34 +28,13 @@ func Test_Start_NotStarted_Started(t *testing.T) {
 	// Arrange
 	watchMock := new(clocktest.WatchMock)
 	watchMock.NowFunc = func() time.Time { return time.Unix(0, 0) }
-	caller := NewActionCaller()
-	function := func(int64) { caller.CallAction() }
-	engine := tick.NewEngine(function, watchMock, 1, 1, 0)
+	var engine = &tick.Engine{}
 	var calls int
-	caller.SetAction(func() {
-		calls++
-		engine.Stop()
-	})
+	engine = tick.NewEngine(func(int64) { calls++; engine.Stop() }, watchMock, 1, 1, 0)
 
 	// Act
 	engine.Start()
 
 	// Assert
 	test.Assert(t, calls == 1, fmt.Sprintf("The function is called %d times whereas it should be called once.", calls))
-}
-
-type ActionsCaller struct {
-	action func()
-}
-
-func NewActionCaller() *ActionsCaller {
-	return &ActionsCaller{}
-}
-
-func (actionsCaller *ActionsCaller) SetAction(action func()) {
-	actionsCaller.action = action
-}
-
-func (actionsCaller *ActionsCaller) CallAction() {
-	actionsCaller.action()
 }
