5375ca7a34c5d93702d16d60c5d81a9de1214573
Jeremy Pansier <jeremypansier@gmail.com>
2024-04-12 15:53:10 +0200

    rename file

--- validatornode/presentation/p2p/neighbor_factory_test.go
+++ validatornode/presentation/p2p/neighbor_factory_test.go
@@ -12,10 +12,10 @@ func Test_CreateSender_IpFinderError_ReturnsNil(t *testing.T) {
 	// Arrange
 	ipFinder := new(net.IpFinderMock)
 	ipFinder.LookupIPFunc = func(string) (string, error) { return "", errors.New("") }
-	senderFactory := NewNeighborFactory(ipFinder, 0)
+	neighborFactory := NewNeighborFactory(ipFinder, 0)
 
 	// Act
-	client, _ := senderFactory.CreateSender("", "0")
+	client, _ := neighborFactory.CreateSender("", "0")
 
 	// Assert
 	test.Assert(t, client == nil, "client is not nil whereas it should be")
@@ -25,10 +25,10 @@ func Test_CreateSender_ValidIp_ReturnsClient(t *testing.T) {
 	// Arrange
 	ipFinder := new(net.IpFinderMock)
 	ipFinder.LookupIPFunc = func(string) (string, error) { return "", nil }
-	senderFactory := NewNeighborFactory(ipFinder, 0)
+	neighborFactory := NewNeighborFactory(ipFinder, 0)
 
 	// Act
-	client, _ := senderFactory.CreateSender("", "0")
+	client, _ := neighborFactory.CreateSender("", "0")
 
 	// Assert
 	test.Assert(t, client != nil, "client is nil whereas it should not")
