4ec6ebaa794f340e12ec908488922d6a6c02cc65
Jeremy Pansier <jeremypansier@gmail.com>
2024-05-17 12:34:36 +0200

    add node test

--- validatornode/application/verification/addresses_registry_test.go
+++ validatornode/application/verification/addresses_registry_test.go
@@ -7,21 +7,31 @@ import (
 )
 
 func Test_Clear_ListsAreNotEmpty_ListsAreCleared(t *testing.T) {
+	// Arrange
 	registry := &AddressesRegistry{
 		registeredAddresses: map[string]bool{"test": true},
 		removedAddresses:    []string{"test"},
 	}
+
+	// Act
 	registry.Clear()
+
+	// Assert
 	test.Assert(t, !registry.registeredAddresses["test"], "registeredAddresses has not been correctly cleared")
 	test.Assert(t, registry.removedAddresses == nil, "removedAddresses has not been correctly cleared")
 }
 
 func Test_Filter_OneOfTwoAddressesIsAlreadyRegistered_ReturnsOneAddress(t *testing.T) {
+	// Arrange
 	registry := &AddressesRegistry{
 		registeredAddresses: map[string]bool{"test": true},
 	}
 	expectedAddress := "new"
+
+	// Act
 	newAddresses := registry.Filter([]string{"test", expectedAddress})
+
+	// Assert
 	actualLength := len(newAddresses)
 	expectedLength := 1
 	test.Assert(t, actualLength == expectedLength, fmt.Sprintf("newAddresses should contain %d item whereas it contains %d", expectedLength, actualLength))
@@ -30,33 +40,53 @@ func Test_Filter_OneOfTwoAddressesIsAlreadyRegistered_ReturnsOneAddress(t *testi
 }
 
 func Test_IsRegistered_Registered_ReturnsTrue(t *testing.T) {
+	// Arrange
 	registry := &AddressesRegistry{
 		registeredAddresses: map[string]bool{"test": true},
 	}
+
+	// Act
 	isRegistered := registry.IsRegistered("test")
+
+	// Assert
 	test.Assert(t, isRegistered, "address is not registered whereas it should be")
 }
 
 func Test_IsRegistered_NotRegistered_ReturnsFalse(t *testing.T) {
+	// Arrange
 	registry := NewAddressesRegistry(nil, nil)
+
+	// Act
 	isRegistered := !registry.IsRegistered("new")
+
+	// Assert
 	test.Assert(t, isRegistered, "address is registered whereas it should not")
 }
 
 func Test_RemovedAddresses_OneAddressRemoved_ReturnsOneAddress(t *testing.T) {
+	// Arrange
 	registry := &AddressesRegistry{
 		removedAddresses: []string{"test"},
 	}
+
+	// Act
 	removedAddresses := registry.RemovedAddresses()
+
+	// Assert
 	test.Assert(t, removedAddresses[0] == "test", "address is not removed whereas it should be")
 }
 
 func Test_Update_AddingAndRemovingAddresses_AddressesCorrectlyUpdated(t *testing.T) {
+	// Arrange
 	registry := &AddressesRegistry{
 		registeredAddresses: map[string]bool{"test": true},
 		removedAddresses:    []string{"test"},
 	}
+
+	// Act
 	registry.Update([]string{"new"}, []string{"test"})
+
+	// Assert
 	test.Assert(t, registry.registeredAddresses["new"], "registeredAddresses has not been correctly updated")
 	test.Assert(t, !registry.registeredAddresses["test"], "registeredAddresses has not been correctly updated")
 	test.Assert(t, len(registry.removedAddresses) == 0, "removedAddresses has not been correctly updated")
--- validatornode/application/verification/utxos_registry_test.go
+++ validatornode/application/verification/utxos_registry_test.go
@@ -9,55 +9,6 @@ import (
 	"testing"
 )
 
-// TODO
-// func Test_UtxosByAddress_UnknownAddress_ReturnsEmptyArray(t *testing.T) {
-//	// Arrange
-//	logger := log.NewLoggerMock()
-//	genesisValidatorAddress := ""
-//	var genesisAmount uint64 = 0
-//	settings := new(validatornode.SettingsProviderMock)
-//	settings.GenesisAmountFunc = func() uint64 { return genesisAmount }
-//	blockchain := NewBlockchain(nil, settings, nil, logger)
-//
-//	// Act
-//	utxosBytes := blockchain.Utxos(genesisValidatorAddress)
-//
-//	// Assert
-//	var utxos []*ledger.Utxo
-//	_ = json.Unmarshal(utxosBytes, &utxos)
-//	test.Assert(t, len(utxos) == 0, "utxos should be empty")
-// }
-//
-// func Test_Utxos_UtxoExists_ReturnsUtxo(t *testing.T) {
-//	// Arrange
-//	registry := new(validatornode.AddressesManagerMock)
-//	registry.IsRegisteredFunc = func(string) (bool, error) { return true, nil }
-//	logger := log.NewLoggerMock()
-//	sendersManagerMock := new(network.SendersManagerMock)
-//	var validationInterval int64 = 1
-//	settings := new(validatornode.SettingsProviderMock)
-//	settings.GenesisAmountFunc = func() uint64 { return 1 }
-//	blockchain := NewBlockchain(registry, settings, sendersManagerMock, logger)
-//	registeredAddress := ""
-//	var expectedValue uint64 = 1
-//	var genesisTimestamp int64 = 0
-//	transaction, _ := ledger.NewRewardTransaction(registeredAddress, true, genesisTimestamp+validationInterval, expectedValue)
-//	transactions := []*ledger.Transaction{transaction}
-//	transactionsBytes, _ := json.Marshal(transactions)
-//	_ = blockchain.AddBlock(genesisTimestamp, nil, nil)
-//	_ = blockchain.AddBlock(genesisTimestamp+validationInterval, transactionsBytes, []string{registeredAddress})
-//	_ = blockchain.AddBlock(genesisTimestamp+2*validationInterval, nil, nil)
-//
-//	// Act
-//	utxosBytes := blockchain.Utxos(registeredAddress)
-//
-//	// Assert
-//	var utxos []*ledger.Utxo
-//	_ = json.Unmarshal(utxosBytes, &utxos)
-//	actualValue := utxos[0].Value(genesisTimestamp+2*validationInterval, 1, 1, 1)
-//	test.Assert(t, actualValue == expectedValue, fmt.Sprintf("utxo amount is %d whereas it should be %d", actualValue, expectedValue))
-// }
-
 func Test_CalculateFee_UnknownTransactionId_ReturnsError(t *testing.T) {
 	// Arrange
 	privateKey, _ := encryption.NewPrivateKeyFromHex(test.PrivateKey)
